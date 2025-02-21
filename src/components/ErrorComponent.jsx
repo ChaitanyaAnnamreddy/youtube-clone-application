950c0d89eb2a3d31b533aa85600221be48ab1173
chaitanya-annamreddy <achaitanya0505@gmail.com>
2025-02-21 21:58:57 +0530

    stying for error component

--- src/components/ErrorComponent.jsx
+++ src/components/ErrorComponent.jsx
@@ -1,7 +1,20 @@
 import { Button, Result } from 'antd'
+import styled from 'styled-components'
+
+const StyledResult = styled(Result)`
+  margin-top: 50px;
+
+  .ant-result-title {
+    color: #f1f1f1 !important;
+  }
+
+  .ant-result-subtitle {
+    color: #aaaaaa !important;
+  }
+`
 
 const ErrorComponent = () => (
-  <Result
+  <StyledResult
     status="404"
     title="404"
     subTitle="Sorry, the page you visited does not exist."
